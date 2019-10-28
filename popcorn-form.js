365bd90bce4a2bd900f3378c2d717cfe0af9a09e
xmos2 <jacob_heard@my.uri.edu>
2019-10-28 16:41:52 -0400

    fixed subtotal function incorrect value alert

--- popcorn-form.js
+++ popcorn-form.js
@@ -1,26 +1,28 @@
 function subtotal() {
-  var UnpoppedQuantity = document.getElementById("unpopped");
+  var UnpoppedQuantity = document.getElementById("unpopped").value;
   var UnpoppedCost= 3 * UnpoppedQuantity;
-
-  var CaramelQuantity = document.getElementById("caramel");
+  var CaramelQuantity = document.getElementById("caramel").value;
   var CaramelCost= 3.5 * CaramelQuantity;
 
-  var CaramelNutQuantity = document.getElementById("caramel-nut");
+  var CaramelNutQuantity = document.getElementById("caramel-nut").value;
   var CaramelNutCost= 4.5 * CaramelNutQuantity;
 
-  var ToffeyQuantity = document.getElementById("toffey");
+  var ToffeyQuantity = document.getElementById("toffey").value;
   var ToffeyCost = 5 * ToffeyQuantity;
 
   var PopcornCost = UnpoppedCost + CaramelCost + CaramelNutCost + ToffeyCost;
+  if (PopcornCost < 3){
+    alert("You entered in an incorrect value");
+  }
   document.getElementById("sub-total").value= PopcornCost;
 }
 
-// function to calculate grand total 
+// function to calculate grand total
 // and any credit card charges
 function grandtotal() {
 	var subtotal = document.getElementsByName("subtotal");
 	var grandtotal = subtotal + subtotal*.07
-	return grandtotal;	
+	return grandtotal;
 }
 
 function submit() {
@@ -28,5 +30,5 @@ function submit() {
 }
 
 function alerts() {
-	alert("Button")
+alert("button")
 }
